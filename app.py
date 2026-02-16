faa35dc0f1521713e53ba3533ca021b2e126ebfa
S.Guna Balan <gunabalan376@gmail.com>
2026-02-16 11:48:19 +0530

    Fix WebSocket production error

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
+import eventlet
+eventlet.monkey_patch()
+
 from flask import Flask, render_template, request, redirect, session
 from flask_socketio import SocketIO, send, join_room, emit
 import sqlite3
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode="eventlet")
+socketio = SocketIO(
+    app,
+    cors_allowed_origins="*",
+    async_mode="eventlet",
+    transports=["polling"]
+)
 
 
 users = {}
